23dbd46baaa33e6423852ee61f506b5d30c0a4fa
AleksandrGlinskiy <tarieboe1993@gmail.com>
2023-09-01 13:50:55 +0300

    add all fetch functions

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,14 +1,32 @@
 const API_KEY = 'dfe97c9693096be016a960b4c69a6096';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-const fetchApi = () => {
-  return fetch(`${BASE_URL}?api_key=${API_KEY}`).then(
-    response => {
-      if (response.ok) {
-        return response.json();
-      } else return Promise.reject(new Error(`not found `));
-    }
+const fetchApi = (url = '') => {
+  return fetch(url).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else return Promise.reject(new Error(`not found `));
+  });
+};
+
+export const fetchTrendingMovies = () => {
+  return fetchApi(`${BASE_URL}/trending/all/day?api_key=${API_KEY}`);
+};
+
+export const fetchSearchMovies = (searchMovie, queryPage) => {
+  return fetchApi(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&page=${queryPage}&query=${searchMovie}`
   );
 };
 
-export default fetchApi;
+export const fetchMovieDetails = movieId => {
+  return fetchApi(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
+};
+
+export const fetchMovieCredits = movieId => {
+  return fetchApi(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`);
+};
+
+export const fetchMovieReviews = movieId => {
+  return fetchApi(`${BASE_URL}/reviews/${movieId}?api_key=${API_KEY}`);
+};
